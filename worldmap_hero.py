c3db182c47f33bf29e0aad20f296a4ce923c33ad
은기 <znfks8819@gmail.com>
2015-12-15 21:12:51 +0900

    hero moving in frame time

--- worldmap_hero.py
+++ worldmap_hero.py
@@ -1,9 +1,12 @@
 __author__ = 'EUNGI'
 from pico2d import *
+import time
 
 hero = None
 
 class Hero:
+    # 0.25초 마다 한 액션을 보여줄 생각
+    TIME_PER_ACTION = 4.0
     def __init__(self):
         self.image = load_image('char\\race2\\worksheet.png')
         self.x, self.y = 508, 600-320
@@ -11,8 +14,8 @@ class Hero:
         self.speed = 10
         self.where = 0
 
-    def update(self):
-        self.frame = (self.frame + 1) % 4
+    def update(self, frame_time):
+        self.frame = int((Hero.TIME_PER_ACTION * frame_time) % 4)
 
     def draw(self):
         self.image.clip_draw(self.frame * 64, 80, 64, 80, self.x, self.y)
@@ -28,8 +31,15 @@ def test_unit():
     open_canvas(1200, 800)
     clear_canvas()
 
-    global  world_background, geo_btn
+    global hero
+    hero = Hero()
+    current_time = time.time()
     while(running):
+        frame_time = time.time() - current_time
+        clear_canvas()
+
+        hero.draw()
+        hero.update(frame_time)
 
         update_canvas()
         events = get_events()
